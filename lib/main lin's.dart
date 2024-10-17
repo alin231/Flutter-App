717d19d7f90b2f0e10d3276d82838fe31c4307bc
Dascupt Ferchmare <Dascupt@Bake-O-Lite-Air.local>
2024-10-17 11:32:11 +0800

    few updates

--- lib/main lin's.dart
+++ lib/main lin's.dart
@@ -1,25 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:myapp/main_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:myapp/main_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        useMaterial3: true,
-      ),
-      home: const MainScreen(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+//         useMaterial3: true,
+//       ),
+//       home: const MainScreen(),
+//     );
+//   }
+// }
 
-//test push
+// //test push
