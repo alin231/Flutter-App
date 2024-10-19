79ca1ee0a835c4dfbeaf38a026276de8a37e2c7a
Dascupt Ferchmare <Dascupt@Bake-O-Lite-Air.local>
2024-10-19 14:02:31 +0800

    add language section

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -2,3 +2,10 @@ const String apiKey = 'AIzaSyAiK-Cz4_YaXD8_gIMArDjs95npTp88AH0';// dfjajd;lfdsjf
 String dailypun = "";
 String normresponse = "";
 String norminput = "";
+
+enum PunLanguage {
+  ZH_CN, // Chinese, 
+  EN,
+  JP,
+  KR
+}
